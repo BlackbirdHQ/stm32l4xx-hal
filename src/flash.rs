042557034748f81f5dadc008ec2665b0fc59b9d6
Mathias <mk@blackbird.online>
2021-03-08 08:33:48 +0100

    Implement the reworked embedded-storage traits for internal flash

--- src/flash.rs
+++ src/flash.rs
@@ -8,7 +8,7 @@
 //!     let mut prog = flash.unlock()?;
 //!     let (start, end) = prog.range();
 //!
-//!     let page = start + Address(1024);
+//!     let page = start + 1024;
 //!
 //!     // Perform the erase and programing operation
 //!     let data = [
@@ -19,7 +19,7 @@
 //!     prog.try_write(page, &data)?;
 //!
 //!     // Check result (not needed, but done for this example)
-//!     let addr = page.0 as *const u64;
+//!     let addr = page as *const u64;
 //!     assert!(unsafe { core::ptr::read(addr) } == data[0]);
 //!     assert!(unsafe { core::ptr::read(addr.offset(1)) } == data[1]);
 //!     assert!(unsafe { core::ptr::read(addr.offset(2)) } == data[2]);
@@ -33,7 +33,11 @@
 use crate::stm32::{flash, FLASH};
 use core::convert::TryInto;
 use core::{mem, ops::Drop, ptr};
-use embedded_storage::{Address, ReadWrite, Region};
+use embedded_storage::{
+    iter::IterableByOverlaps,
+    nor_flash::{NorFlash, ReadNorFlash},
+    Region,
+};
 
 #[derive(Clone, Copy, Debug)]
 /// Size of the MCU flash
@@ -65,21 +69,87 @@ pub enum FlashVariant {
 }
 
 impl FlashVariant {
-    const fn bytes(self) -> u32 {
-        self as u32 * 1024
+    const fn bytes(self) -> usize {
+        self as usize * 1024
     }
 }
 
+/// Error type of flash peripheral when programming
+/// If an error occurs during a program or erase operation, one of the following error flags is set
+#[derive(Debug)]
+pub enum ProgrammingError {
+    /// Programming Error
+    ///
+    /// In standard programming: PROGERR is set if the word to write is not previously erased
+    /// (except if the value to program is full zero).
+    Programming,
+    /// Size Programming Error
+    ///
+    /// In standard programming or in fast programming: only double word can be
+    /// programmed and only 32-bit data can be written. SIZERR is set if a byte or an halfword is written
+    Size,
+    /// Alignment Programming error
+    ///
+    /// PGAERR is set if one of the following conditions occurs:
+    /// – In standard programming: the first word to be programmed is not aligned with a
+    ///   double word address, or the second word doesn’t belong to the same double word
+    ///   address.
+    /// – In fast programming: the data to program doesn’t belong to the same row than the
+    ///   previous programmed double words, or the address to program is not greater than
+    ///   the previous one.
+    Alignment,
+    /// Programming Sequence Error
+    ///
+    /// PGSERR is set if one of the following conditions occurs:
+    /// – In the standard programming sequence or the fast programming sequence: a data
+    ///   is written when PG and FSTPG are cleared.
+    /// – In the standard programming sequence or the fast programming sequence:
+    ///   MER1, MER2, and PER are not cleared when PG or FSTPG is set.
+    /// – In the fast programming sequence: the Mass erase is not performed before setting
+    ///   FSTPG bit.
+    /// – In the mass erase sequence: PG, FSTPG, and PER are not cleared when MER1
+    ///   or MER2 is set.
+    /// – In the page erase sequence: PG, FSTPG, MER1 and MER2 are not cleared when
+    ///   PER is set.
+    /// – PGSERR is set also if PROGERR, SIZERR, PGAERR, MISSERR, FASTERR or
+    ///   PGSERR is set due to a previous programming error
+    Sequence,
+    /// Write Protection Error
+    ///
+    /// WRPERR is set if one of the following conditions occurs:
+    /// – Attempt to program or erase in a write protected area (WRP) or in a PCROP area.
+    /// – Attempt to perform a bank erase when one page or more is protected by WRP or
+    ///   PCROP.
+    /// – The debug features are connected or the boot is executed from SRAM or from
+    ///   System flash when the read protection (RDP) is set to Level 1.
+    /// – Attempt to modify the option bytes when the read protection (RDP) is set to
+    ///   Level 2.
+    WriteProtection,
+    /// Fast Programming Data Miss Error
+    ///
+    /// In fast programming: all the data must be written successively. MISSERR is set if the
+    /// previous data programmation is finished and the next data to program is not written yet
+    DataMiss,
+    /// Fast Programming Error
+    ///
+    /// In fast programming: FASTERR is set if one of the following conditions occurs:
+    /// – When FSTPG bit is set for more than 7ms which generates a time-out detection.
+    /// – When the row fast programming has been interrupted by a MISSERR, PGAERR,
+    ///   WRPERR or SIZERR.
+    FastProgramming,
+}
+
 /// Error type of flash peripheral
+#[derive(Debug)]
 pub enum Error {
     /// Flash controller is not done yet
     Busy,
     /// Error detected (by command execution, or because no command could be executed)
-    Illegal,
+    Programming(ProgrammingError),
     /// Set during read if ECC decoding logic detects correctable or uncorrectable error
     EccError,
     /// Page number is out of range
-    PageOutOfRange,
+    OutOfBounds,
     /// (Legal) command failed
     Failure,
 }
@@ -195,6 +265,11 @@ impl McuFlash {
             Err(Error::Failure)
         }
     }
+
+    /// Return the capacity of the flash in bytes
+    pub fn capacity(&self) -> usize {
+        self.memory_map.0.bytes() - 1
+    }
 }
 
 /// Flash programming interface
@@ -211,11 +286,15 @@ impl<'a> FlashProgramming<'a> {
         if sr.bsy().bit_is_set() {
             Err(Error::Busy)
         } else if sr.pgaerr().bit_is_set() {
-            Err(Error::Illegal)
+            Err(Error::Programming(ProgrammingError::Alignment))
         } else if sr.progerr().bit_is_set() {
-            Err(Error::Illegal)
+            Err(Error::Programming(ProgrammingError::Programming))
         } else if sr.wrperr().bit_is_set() {
-            Err(Error::Illegal)
+            Err(Error::Programming(ProgrammingError::WriteProtection))
+        } else if sr.sizerr().bit_is_set() {
+            Err(Error::Programming(ProgrammingError::Size))
+        } else if sr.pgserr().bit_is_set() {
+            Err(Error::Programming(ProgrammingError::Sequence))
         } else {
             Ok(())
         }
@@ -234,19 +313,44 @@ impl<'a> FlashProgramming<'a> {
 
     /// Erase all flash pages, note that this will erase the current running program if it is not
     /// called from a program running in RAM.
-    pub fn erase_all_pages(&mut self) -> Result<(), Error> {
-        self.cr.cr().modify(|_, w| w.mer1().set_bit());
+    pub fn erase_banks(&mut self, banks: &[MemoryBank]) -> Result<(), Error> {
+        self.status()?;
+
+        // Set the MER1 bit or/and MER2 (depending on the bank) in the Flash control register
+        // (FLASH_CR). Both banks can be selected in the same operation.
+        self.cr.cr().modify(|_, w| {
+            for bank in banks {
+                match bank {
+                    MemoryBank::Bank1 => w.mer1().set_bit(),
+                    MemoryBank::Bank2 => w.mer2().set_bit(),
+                };
+            }
+            w
+        });
+
+        // Set the STRT bit in the FLASH_CR register.
         self.cr.cr().modify(|_, w| w.start().set_bit());
 
+        // Wait for the BSY bit to be cleared in the Flash status register (FLASH_SR)
         let res = self.wait();
 
-        self.cr.cr().modify(|_, w| w.mer1().clear_bit());
+        self.cr.cr().modify(|_, w| {
+            for bank in banks {
+                match bank {
+                    MemoryBank::Bank1 => w.mer1().clear_bit(),
+                    MemoryBank::Bank2 => w.mer2().clear_bit(),
+                };
+            }
+            w
+        });
 
         res
     }
 
     fn erase_page(&mut self, page: &Page) -> Result<(), Error> {
-        let page_number = (MemoryMap::start() + page.location).0 / page.size as u32;
+        self.status()?;
+
+        let page_number = (page.location - MemoryMap::start()) / page.size as u32;
         match page.area {
             Area::Main(MemoryBank::Bank1) => {
                 self.cr.cr().modify(|_, w| unsafe {
@@ -270,7 +374,7 @@ impl<'a> FlashProgramming<'a> {
             }
             // TODO: Handle Area::SystemMemory, Area::OneTimeProgrammable & Area::OptionBytes
             _ => {
-                return Err(Error::PageOutOfRange);
+                return Err(Error::OutOfBounds);
             }
         }
 
@@ -283,7 +387,16 @@ impl<'a> FlashProgramming<'a> {
         res
     }
 
+    /// Programming in a previously programmed address is not allowed except if the data to write
+    /// is full zero, and any attempt will set PROGERR flag in the Flash status register
+    /// (FLASH_SR).
+    ///
+    /// It is only possible to program double word (2 x 32-bit data).
+    /// • Any attempt to write byte or half-word will set SIZERR flag in the FLASH_SR register.
+    /// • Any attempt to write a double word which is not aligned with a double word address will
+    ///   set PGAERR flag in the FLASH_SR register.
     fn write_native(&mut self, data: &[u64], aligned_address: usize) -> Result<(), Error> {
+        self.status()?;
         // NB: The check for alignment of the address, and that the flash is erased is made by the
         // flash controller. The `wait` function will return the proper error codes.
         let mut address = aligned_address as *mut u32;
@@ -310,21 +423,21 @@ impl<'a> FlashProgramming<'a> {
         Ok(())
     }
 
-    fn write_bytes(&mut self, data: &[u8], address: Address) -> Result<(), Error> {
-        let address = address.0 as usize;
+    fn write_bytes(&mut self, data: &[u8], address: u32) -> Result<(), Error> {
+        let address = address as usize;
         let address_offset = address % mem::align_of::<u64>();
         let unaligned_size = (mem::size_of::<u64>() - address_offset) % mem::size_of::<u64>();
 
         if unaligned_size > 0 {
             let unaligned_data = &data[..unaligned_size];
             // Handle unaligned address data, make it into a native write
-            let mut data = 0xffff_ffff_ffff_ffffu64;
+            let mut dword = 0xffff_ffff_ffff_ffffu64;
             for b in unaligned_data {
-                data = (data >> 8) | ((*b as u64) << 56);
+                dword = (dword >> 8) | ((*b as u64) << 56);
             }
 
             let unaligned_address = address - address_offset;
-            let native = &[data];
+            let native = &[dword];
             self.write_native(native, unaligned_address)?;
         }
 
@@ -348,13 +461,13 @@ impl<'a> FlashProgramming<'a> {
         let rem = chunks.remainder();
 
         if !rem.is_empty() {
-            let mut data = 0xffff_ffff_ffff_ffffu64;
+            let mut dword = 0xffff_ffff_ffff_ffffu64;
             // Write remainder
             for b in rem.iter().rev() {
-                data = (data << 8) | *b as u64;
+                dword = (dword << 8) | *b as u64;
             }
 
-            let native = &[data];
+            let native = &[dword];
             self.write_native(native, aligned_address)?;
         }
 
@@ -384,7 +497,7 @@ enum Area {
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 /// Describes which memory bank is being addressed
-enum MemoryBank {
+pub enum MemoryBank {
     /// Memory bank 1
     Bank1,
     /// Memory bank 2
@@ -395,7 +508,7 @@ enum MemoryBank {
 /// Flash page, as being the smallest entity to erase
 struct Page {
     area: Area,
-    location: Address,
+    location: u32,
     size: usize,
 }
 
@@ -403,16 +516,16 @@ const MAX_PAGE_SIZE: usize = 2048;
 
 #[derive(Clone, Copy)]
 /// Memory map describing the sections and pages of the flash
-struct MemoryMap(FlashVariant);
+pub struct MemoryMap(FlashVariant);
 
 impl MemoryMap {
     /// Get an iterator over all the possible pages of the flash memory
     fn pages(&self) -> impl Iterator<Item = Page> {
-        let page_size = 2048;
+        let page_size = self.page_size();
         let last_page = (self.0.bytes() / page_size) - 1;
 
         (0..last_page).map(move |page_number| {
-            let bank = if page_number < last_page / 2 {
+            let bank = if page_number <= last_page / 2 {
                 MemoryBank::Bank1
             } else {
                 MemoryBank::Bank2
@@ -420,32 +533,36 @@ impl MemoryMap {
 
             Page::new(
                 Area::Main(bank),
-                Self::start() + Address(page_number * page_size),
+                Self::start() + (page_number * page_size) as u32,
                 page_size as usize,
             )
         })
     }
 
     /// The base address (start) of the flash memory
-    pub const fn start() -> Address {
-        Address(0x0800_0000)
+    pub const fn start() -> u32 {
+        0x0800_0000
     }
 
     /// The end address of the flash memory
-    pub const fn end(&self) -> Address {
-        Address(0x0800_0000 + self.0.bytes() - 1)
+    pub const fn end(&self) -> u32 {
+        (0x0800_0000 + self.0.bytes() - 1) as u32
+    }
+
+    fn page_size(&self) -> usize {
+        MAX_PAGE_SIZE
     }
 }
 
 impl Region for Page {
-    fn contains(&self, address: Address) -> bool {
+    fn contains(&self, address: u32) -> bool {
         (self.location <= address) && (self.end() > address)
     }
 }
 
 impl Page {
     /// Construct a new `Page`
-    const fn new(area: Area, location: Address, size: usize) -> Self {
+    const fn new(area: Area, location: u32, size: usize) -> Self {
         Self {
             area,
             location,
@@ -454,16 +571,22 @@ impl Page {
     }
 
     /// The end address of the page
-    const fn end(&self) -> Address {
-        Address(self.location.0 + self.size as u32 - 1)
+    const fn end(&self) -> u32 {
+        self.location + self.size as u32 - 1
     }
 }
 
-impl<'a> ReadWrite for FlashProgramming<'a> {
+impl<'a> ReadNorFlash for FlashProgramming<'a> {
     type Error = Error;
 
-    fn try_read(&mut self, address: Address, bytes: &mut [u8]) -> nb::Result<(), Self::Error> {
-        let mut address = address.0 as *const u8;
+    const READ_SIZE: usize = 1;
+
+    fn try_read(&mut self, offset: u32, bytes: &mut [u8]) -> Result<(), Self::Error> {
+        if offset > self.capacity() as u32 {
+            return Err(Error::OutOfBounds);
+        }
+
+        let mut address = (MemoryMap::start() + offset) as *const u8;
 
         for data in bytes {
             unsafe {
@@ -474,39 +597,48 @@ impl<'a> ReadWrite for FlashProgramming<'a> {
         Ok(())
     }
 
-    fn try_write(&mut self, address: Address, bytes: &[u8]) -> nb::Result<(), Self::Error> {
-        self.write_bytes(bytes, address)?;
-        // for (block, page, addr) in self.memory_map.pages().overlaps(bytes, address) {
-        //     let merge_buffer = &mut [0u8; MAX_PAGE_SIZE][0..page.size];
-        //     let offset_into_page = addr.0.saturating_sub(page.location.0) as usize;
+    fn capacity(&self) -> usize {
+        self.memory_map.0.bytes() - 1
+    }
+}
 
-        //     self.try_read(page.location, merge_buffer)?;
+impl<'a> NorFlash for FlashProgramming<'a> {
+    const WRITE_SIZE: usize = 8;
+    const ERASE_SIZE: usize = 2048;
 
-        //     if block.is_subset_of(&merge_buffer[offset_into_page..page.size]) {
-        //         self.write_bytes(block, addr)?;
-        //     } else {
-        //         self.erase_page(&page)?;
-        //         merge_buffer
-        //             .iter_mut()
-        //             .skip(offset_into_page)
-        //             .zip(block)
-        //             .for_each(|(byte, input)| *byte = *input);
-        //         self.write_bytes(merge_buffer, page.location)?;
-        //     }
-        // }
+    fn try_write(&mut self, offset: u32, bytes: &[u8]) -> Result<(), Self::Error> {
+        if offset as usize + bytes.len() > self.capacity() {
+            return Err(Error::OutOfBounds);
+        }
+
+        if offset % Self::WRITE_SIZE as u32 != 0 {
+            return Err(Error::Programming(ProgrammingError::Alignment));
+        }
+
+        if bytes.len() % Self::WRITE_SIZE != 0 {
+            return Err(Error::Programming(ProgrammingError::Size));
+        }
+
+        let mut aligned_address = (MemoryMap::start() + offset) as usize;
+        for exact_chunk in bytes.chunks_exact(Self::WRITE_SIZE) {
+            let native = &[u64::from_ne_bytes(exact_chunk.try_into().unwrap())];
+            self.write_native(native, aligned_address)?;
+            aligned_address += Self::WRITE_SIZE;
+        }
 
         Ok(())
     }
 
-    fn range(&self) -> (Address, Address) {
-        (MemoryMap::start(), self.memory_map.end())
-    }
+    fn try_erase(&mut self, from: u32, to: u32) -> Result<(), Self::Error> {
+        // Check that from & to is properly aligned to a proper erase resolution
+        if from % 2048 != 0 || to % 2048 != 0 {
+            return Err(Error::Programming(ProgrammingError::Alignment));
+        }
 
-    fn try_erase(&mut self, from: Address, to: Address) -> nb::Result<(), Self::Error> {
         self.memory_map
             .pages()
-            .skip_while(|page| !page.contains(from))
-            .take_while(|page| page.contains(to))
-            .try_for_each(|page| self.erase_page(&page).map_err(nb::Error::Other))
+            .skip_while(|page| !page.contains(MemoryMap::start() + from))
+            .take_while(|page| !page.contains(MemoryMap::start() + to))
+            .try_for_each(|page| self.erase_page(&page))
     }
 }
